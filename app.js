61b81f4705ea66c3515c7b99533f9c6f1efb47e7
袁理开 <1091372693@qq.com>
2020-12-16 15:37:15 +0800

    1

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // 配置路由
-app.use('/index', indexRouter);
+app.use('/api', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const crypto = require('crypto');
 const router = express.Router();
 
 // const sql = require('../public/javascripts/mysql');
@@ -6,27 +7,40 @@ const router = express.Router();
 /* GET home page. */
 
 router.all('/index', function (req, res, next) {
+	function Decrypted(data, key) {
+		const decipher = crypto.createDecipher('aes-256-cbc', key);
+		var decrypted = decipher.update(data, 'hex', 'utf8');
+		decrypted += decipher.final('utf8');
+		return decrypted
+	}
+	var data = '83fc779b49fe50519100ae885db8c65d';
+	var key = 'InmbuvP6Z8';
+	var decrypted = Decrypted(data, key)
+	res.json({
+		status: 200,
+		data: {
+			Plain: data,
+			Decrypted: decrypted
+		}
+	});
 
-
-    res.json({status: 201, Message: "未查询到数据"});
-
-    // sql.query('SELECT * FROM city WHERE ID in (' + req.body.ID + ');', function (err, rows) {
-    //
-    //     if (rows.length < 1) {
-    //         res.json({status: 201, Message: "未查询到数据"});
-    //     } else {
-    //         res.json({status: 200, message: "success", data: rows});
-    //     }
-    // });
-    //
-    // var p = new Promise(function(resolve,reject){
-    //     console.log('123')
-    //     resolve()
-    // }).then(
-    //     ()=> console.log('图片加载成功')
-    // ).catch(
-    //     ()=> console.log('图片加载失败')
-    // )
+	// sql.query('SELECT * FROM city WHERE ID in (' + req.body.ID + ');', function (err, rows) {
+	//
+	//     if (rows.length < 1) {
+	//         res.json({status: 201, Message: "未查询到数据"});
+	//     } else {
+	//         res.json({status: 200, message: "success", data: rows});
+	//     }
+	// });
+	//
+	// var p = new Promise(function(resolve,reject){
+	//     console.log('123')
+	//     resolve()
+	// }).then(
+	//     ()=> console.log('图片加载成功')
+	// ).catch(
+	//     ()=> console.log('图片加载失败')
+	// )
 });
 
 module.exports = router;
